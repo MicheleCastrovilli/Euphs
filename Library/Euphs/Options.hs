f4bdcfe84d1349544672fe8eba5e3ac26ab3a0a5
Michele Castrovilli <michele.castrovilli@gmail.com>
2015-09-30 15:59:21 +0200

    Proper commandline parsing options

---
+++ Library/Euphs/Options.hs
@@ -0,0 +1,75 @@
+module Options
+    ( Opts(..)
+    , parseOpts
+    , showUsage
+    , showUsageAndExit
+    ) where
+
+import System.Exit
+import System.Console.GetOpt
+import System.Environment
+
+
+data Opts = Opts { heimHost   :: String    -- ^ Heim instance hoster
+                 , heimPort   :: Int       -- ^ The port to connect to
+                 , sslPort    :: Bool      -- ^ Whether to use wss or ws
+                 , roomList   :: String    -- ^ A list of initial rooms separated by whitespace
+                 , showHelp   :: Bool      -- ^ Print help on startup
+                 , logTarget  :: FilePath    -- ^ The log file to write to.
+                                           -- Fall back to 'stdout' when
+                                           -- empty
+                 , botAccount :: FilePath  -- ^ Email and password for logging into the account
+                 , nick       :: String    -- ^ The nick the Bot will be using.
+                 , config     :: FilePath  -- ^ Config file for specific bot options
+                 }
+                 deriving (Show)
+
+defaults :: Opts
+defaults = Opts { heimHost   = "euphoria.io"
+                , heimPort   = 443
+                , sslPort    = True
+                , roomList   = "test"
+                , showHelp   = False
+                , logTarget  = ""
+                , botAccount = ""
+                , nick       = "EmptyBot"
+                , config     = ""
+                }
+
+
+options :: [OptDescr (Opts -> Opts)]
+options =
+    [ Option "e" ["host"]
+        (ReqArg (\arg opt -> opt {heimHost = arg}) "HOST") "Heim instance to connect to"
+    , Option "p" ["port"]
+        (ReqArg (\arg opt -> opt {heimPort = read arg :: Int}) "PORT") "Port to use"
+    , Option "r" ["rooms", "room"]
+        (ReqArg (\arg opt -> opt {roomList = arg}) "ROOMS") "Rooms to join"
+    , Option "s" ["nossl"]
+        (NoArg (\opt -> opt {sslPort = False})) "Disable SSL"
+    , Option "l" ["log"]
+        (ReqArg (\arg opt -> opt {logTarget = arg}) "LOG") "Logging FilePath"
+    , Option "i" ["identity"]
+        (ReqArg (\arg opt -> opt {botAccount = arg}) "ID") "Bot Identity FilePath"
+    , Option "c" ["config"]
+        (ReqArg (\arg opt -> opt {config = arg}) "CONFIG") "Path for the bot config file"
+    , Option "n" ["nick"]
+        (ReqArg (\arg opt -> opt {nick = arg}) "NICK") "Nickname to use"
+    , Option "h" ["help"]
+        (NoArg (\opt -> opt {showHelp = True})) "Shows this help."
+    ]
+
+header = "Usage: Euphs [options]"
+
+parseOpts :: [String] -> IO (Opts, [String])
+parseOpts argv =
+    case getOpt Permute options argv of
+        (o,n,[]  ) -> return (foldl (flip id) defaults o, n)
+        (_,_,errs) -> ioError (userError (concat errs ++ showUsage))
+
+
+showUsage = usageInfo header options
+
+showUsageAndExit = do
+        putStrLn showUsage
+        exitSuccess
